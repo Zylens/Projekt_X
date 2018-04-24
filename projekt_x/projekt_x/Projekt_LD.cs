b6ef286a9055a25518bc2963da6218af5140c42d
Zylens <Zylens@gmx.net>
2018-04-24 09:22:57 +0200

    program test
    program test

--- projekt_x/projekt_x/Projekt_LD.cs
+++ projekt_x/projekt_x/Projekt_LD.cs
@@ -8,5 +8,24 @@ namespace projekt_x
 {
     public class Projekt_LD
     {
+        public static List<int> Start_Zahlen_testen(int untergrenze, int obergrenze)
+        {
+            var zahl = Zahlen_testen(untergrenze, obergrenze);
+            return zahl;
+        }
+        internal static List<int> Zahlen_testen (int untergrenze, int obergrenze)
+        {
+            var zahlen = new List<int>();
+            for (int z = untergrenze; z <= obergrenze; z++)
+            {
+
+                zahlen.Add(z);
+            }
+
+            return zahlen;
+
+
+        }
+
     }
 }
--- projekt_x_test/Projekt_LD_test.cs
+++ projekt_x_test/Projekt_LD_test.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using projekt_x;
 
 namespace projekt_x_test
 {
+     [TestFixture]
     public class Projekt_LD_test
     {
+        [Test, Category("Akzeptanztest")]
+        public void Test_zahlen()
+        {
+            var ergebnis= Projekt_LD.Start_Zahlen_testen(2,15);
+            Assert.AreEqual(new[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 },ergebnis.ToArray());
+        }
+        [Test, Category("Gerüsttest")]
+        public void Anzahl_zahlen()
+        {
+            var ergebnis = Projekt_LD.Zahlen_testen(-1, 100);
+            Assert.AreEqual(102, ergebnis.Count());
+
+        }
+
     }
+
 }
