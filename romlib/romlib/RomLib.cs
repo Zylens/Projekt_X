b7c0061bb853421c8a6d093edf4196e78dbdd562
Zylens <Zylens@gmx.net>
2018-05-02 11:54:02 +0200

    RomLib
    add RomLib

---
+++ romlib/romlib/RomLib.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace romlib
+{
+    public class RomLib
+    {
+        internal static string NumberInRom(int value)
+        {
+            if ((value < 1) || (value >= Int32.MaxValue)) { return ""; }
+            string res = "";
+
+            while (value >= 1000) { value -= 1000; res += "M"; }
+            if (value >= 900) { value -= 900; res += "CM"; }
+
+            while (value >= 500) { value -= 500; res += "D"; }
+            if (value >= 400) { value -= 400; res += "CD"; }
+
+            while (value >= 100) { value -= 100; res += "C"; }
+            if (value >= 90) { value -= 90; res += "XC"; }
+
+            while (value >= 50) { value -= 50; res += "L"; }
+            if (value >= 40) { value -= 40; res += "XL"; }
+
+            while (value >= 10) { value -= 10; res += "X"; }
+            if (value >= 9) { value -= 9; res += "IX"; }
+
+            while (value >= 5) { value -= 5; res += "V"; }
+            if (value >= 4) { value -= 4; res += "IV"; }
+
+            while (value >= 1) { value -= 1; res += "I"; }
+
+            return res;
+        }
+
+
+        internal static string statement(int rom)
+        {
+            string result = "Hier ist was falschgelaufen!";
+            {
+                result = (NumberInRom(rom));
+                return result;
+            }
+            
+            
+        }
+    }
+}
---
+++ test_romlib/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using romlib;
+
+namespace test_romlib
+{
+    public class Test
+    {
+        [TestFixture]
+        public class RomLibTest
+        {
+            [Test, Category("Akzeptanztest")]
+            public void Test_zahlen()
+            {
+                var ergebnis = RomLib.NumberInRom(3500);
+                Assert.AreEqual("MMMD", ergebnis);
+            }
+            [Test, Category("Gerüsttest")]
+            public void Anzahl_zahlen()
+            {
+                var ergebnis = RomLib.statement(7);
+                Assert.AreEqual("VII", ergebnis);
+
+            }
+        }
+    }
+}
